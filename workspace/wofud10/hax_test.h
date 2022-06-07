3222f48985abaac97d7f292af102b40a209f7f9c
Hwang <wofud10@example.com>
2022-06-07 14:54:15 +0900

    haxtest

---
+++ workspace/wofud10/hax_test.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int haxdecode(char *s,int arraySize, char* res);
+int haxencode(char *s, int arraySize, char *res);
+int binaryencode(char *s, int arraySize, char *res);
+int binarydecode(char *s, int arraySize, char* res);
+
