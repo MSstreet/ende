7c3b2ac53a5a1abc270811b739b5dd7d9173a518
jwshim <jwshim@secucen.com>
2022-06-07 14:56:36 +0900

    hell

--- workspace/hax_test.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
-int haxdecode(char *s,int arraySize, char* res);
-int haxencode(char *s, int arraySize, char *res);
-int binaryencode(char *s, int arraySize, char *res);
-int binarydecode(char *s, int arraySize, char* res);
-
--- workspace/jwshim/test.c
+++ workspace/jwshim/test.c
@@ -2,5 +2,15 @@
 
 void main ()
 {
+	char * hello = "hello";
+	int iter = 0;
+	for (;iter<5;iter++)
+		printf ("%c\n", hello[iter]);
+	printf ("\n");
+	
+	for (iter = 0;iter<5;iter++)
+		printf ("%X\n", hello[iter]);
+	printf ("\n");
+
 	printf ("hello world");
 }
